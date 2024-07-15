7b5aedb4e9a3d510707a7b9a42e02279edd82f86
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2024-07-16 00:05:31 +0200

    changes

--- XX - lifecycle/src/main.ts
+++ XX - lifecycle/src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: []
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
