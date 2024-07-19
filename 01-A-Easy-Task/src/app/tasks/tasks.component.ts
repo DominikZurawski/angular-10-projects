474b06e5f02bf9f424000238a7098be8cc97b0a3
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2024-07-19 12:44:31 +0200

    Corrects commits

---
+++ 01-A-Easy-Task/src/app/tasks/tasks.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type  NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent, NewTaskComponent],
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name?: string;
+  isAddingTask = false;
+
+
+  constructor(private taskService: TasksService){
+    this.taskService = taskService;
+
+  }
+  // @Input({ required: true }) name: string | undefined;
+
+
+  get selectedUserTasks() {
+    return this.taskService.getUserTasks(this.userId);
+  }
+
+//   onCompleteTask(id: string) {
+    
+//   }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+
+//   onAddTask(taskData: NewTaskData ){
+    
+//     this.isAddingTask = false;
+//   }
+}
---
+++ 01-B-easy-task-module/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+//   @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enterdSummary = '';
+  eneterdDate = '';
+  private tasksService = inject(TasksService);
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.tasksService.addTask({
+      title: this.enteredTitle,
+      summary: this.enterdSummary,
+      date: this.eneterdDate,
+    },
+this.userId
+);
+    // this.add.emit({
+    //     title: this.enteredTitle,
+    //     summary: this.enterdSummary,
+    //     date: this.eneterdDate,
+    //   });
+  }
+}
